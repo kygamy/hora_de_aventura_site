291c430d33d3936b12900b55937e6603fc0aea45
Victor Gabriel <vgabrielcunhabezerra@gmail.com>
2024-04-15 14:17:49 -0300

    atualizaçâo 1.2

--- setup/urls.py
+++ setup/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('galeria.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("galeria.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
